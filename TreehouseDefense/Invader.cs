95cbbaa9435d67757b04589ae64741137a8a0c7f
John Amann <johnaman01@gmail.com>
2017-02-04 10:15:25 -0500

    Add project files.

---
+++ TreehouseDefense/Invader.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreehouseDefense
+{
+    class Invader
+    {
+        //initialize fields before constructor is called
+        private readonly Path _path;
+        private int _pathStep = 0;
+        //AUTOPROPERTY
+        public MapLocation Location { get; private set; }
+        //constructor
+        public Invader(Path path)
+        {
+            _path = path;
+            Location = path.GetLocationAt(_pathStep);
+        }
+        //how the invader moves down the path
+        public void Move()
+        {
+            _pathStep += 1;
+            Location = _path.GetLocationAt(_pathStep);
+        }
+    }
+}
---
+++ TreehouseDefense/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreehouseDefense
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Map map = new TreehouseDefense.Map(8, 5);
+            try
+            {
+                Path path = new Path(
+                    new []
+                    {
+                         new MapLocation(0, 2),
+                         new MapLocation(1, 2),
+                         new MapLocation(2, 2),
+                         new MapLocation(3, 2),
+                         new MapLocation(4, 2),
+                         new MapLocation(5, 2),
+                         new MapLocation(6, 2),
+                         new MapLocation(7, 2)
+                    });
+               
+               
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
